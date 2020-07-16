52926b3a88a83b3446e5f7c3a3c02843cb939f9b
Anmol Singh <anmolsingh1597@gmail.com>
2020-07-15 20:00:37 -0400

    [Anmol Singh]Database: Table name chnaged from contact to Contact

--- app/src/main/java/com/lambton/contact_anmol_c0777245_android/roomDatabase/ContactInfo.java
+++ app/src/main/java/com/lambton/contact_anmol_c0777245_android/roomDatabase/ContactInfo.java
@@ -5,7 +5,7 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "contact")
+@Entity(tableName = "Contact")
 public class ContactInfo {
 
         @PrimaryKey(autoGenerate = true)
